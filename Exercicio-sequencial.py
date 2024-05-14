6d69330d65eb904056274d4bf390e9055b627f7a
Caio Ursulino Lemos Melo <caioursulino8@gmail.com>
2024-05-14 17:52:25 -0300

    Estudos Python

---
+++ Exercicio-sequencial.py
@@ -0,0 +1,9 @@
+x = int(input('Primeiro Valor:'))
+y = int(input('Segundo Valor'))
+
+if x > y:
+    print('O Primeiro' + str(x) + 'é maior que o Segundo')
+elif x < y:
+    print('O Segundo' + str(y) + 'é maior que o Primeiro')
+else:
+    print('São iguais')
